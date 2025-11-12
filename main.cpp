68eb8d05189be0acaf0b51860f04e3211400f9a0
seift340-dot <seift340@gmail.com>
2025-11-12 15:38:17 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,44 @@
-#include "Animal.h"
-#include "Bird.h"
-#include "Mammal.h"
-#include "Reptile.h"
-#include "Enclosure.h"
-#include "Visitor.h"
+#endif
+Source File:
 #include <iostream>
-using namespace std;
+#include "Animal.h"
+void Animal::display1((string name, int age ,bool isHungry) {
+this->name = name;
+this->age = age;
+this->isHungry = isHungry;
+void Animal::display2() {
+cout << "name" << make << endl;
+cout << "age " << age << endl;
+cout << "ishungry " << ishungry << endl;
 
-
-// ============== MAIN FUNCTION ==============
-int main() {
+}
+}
+void Animal::feed(bool isHungry)
+if Animal(animal==isHungry ){ded
+return YES}
+else {return 0 }
+#endif
+Source File:
+#include <iostream>
+#include "Enclosure.h"
+void Enclosure :: addAnimal (Animal*a){
     
     
-    return 0;
+    
+    
+}
+void Enclosure :: displayAnimals( string mammal ,string bird , stringreptitr){
+
+
+
+}
+
+
+#endif
+Source File:
+#include <iostream>
+#include "Vistor.h"
+void visitor::displayInfo(string visitorname, int ticketsbought ) {
+cout << "visitorname" << visitorname << endl;
+cout << "ticketsbought " << ticketsbought << endl;
 }
