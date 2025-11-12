6777750ecfc6971356fd317baf16ed83fac77ef0
seift340-dot <seift340@gmail.com>
2025-11-12 15:29:47 +0200

    Update Animal.h

--- Animal.h
+++ Animal.h
@@ -1,11 +1,35 @@
-#ifndef ANIMAL_H
-#define ANIMAL_H
+#ifndef ANIMAL_H_INCLUDED
+#define ANIMAL_H_INCLUDED
 
-#include <string>
+
+
+#endif // ANIMAL_H_INCLUDED
 using namespace std;
-
-//define animal class here 
-
-
-
-#endif
+class Animal {
+public:
+void initialize(string name, int age ,bool isHungry);
+void display();
+void feed()
+private:
+string name;
+int age;
+bool isHungry ;
+};
+ mammal : mammal{
+private :
+  string  mammal = furColor ;
+ mammal : mammal (string name, int age ,bool isHungry)
+ {
+   this->furcolor = furcolor
+ }
+};
+ bird : bird{
+bird : bird(string name, int age ,bool isHungry)
+ {
+   this->wingspan = wingspan
+ }
+Reptile :: Reptile (){
+isVenomus = false ;
+Reptile :: Reptile (string name, int age ,bool isHungry){
+this->isVenomus = isVenomus;
+}
