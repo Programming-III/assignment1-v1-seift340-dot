8a5de88810a8120789ffcffbd0d04ca1e54b319d
seift340-dot <seift340@gmail.com>
2025-11-12 15:35:10 +0200

    Update Enclosure.h

--- Enclosure.h
+++ Enclosure.h
@@ -1,15 +1,20 @@
-#ifndef ENCLOSURE_H
-#define ENCLOSURE_H
-
-#include "Animal.h"
-
-//define enclosure class here 
+#ifndef ENCLOSURE_H_INCLUDED
+#define ENCLOSURE_H_INCLUDED
 
 
 
+#endif // ENCLOSURE_H_INCLUDED
+using namespace std;
+class Enclosure {
+public:
 
+void addAnimal();
+void displayAnimals();
+private:
+    Enclosure()
+Enclosure( animal = arr ,int  currentcapacity, int currentcount)
+virtual Enclosure ( )
+int capacity;
+int currentCount;
 
-
-
-
-#endif
+};
